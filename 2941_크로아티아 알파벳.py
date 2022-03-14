d9d7c409ce92a0430f1f1855d0fc89f74044619b
DESKTOP-UUMVHS4\Admin <kma7574@gmail.com>
2022-03-14 17:51:17 +0900

    중복파일 제거

--- 2941_크로아티아 알파벳.py
+++
@@ -1,9 +0,0 @@
-import sys
-data = sys.stdin.readline().rstrip()
-
-croatia = ['c=','c-','dz=','d-','lj','nj','s=','z=']
-
-for i in croatia:
-    data = data.replace(i, '@')
-print(len(data))
-        
