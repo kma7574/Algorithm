15c320bfcf11a966e634693d9db4674aef160a80
DESKTOP-69R4QNC\JungJaeYun <kma7574@naver.com>
2022-07-11 09:00:01 +0900

    2022-07-03 알고리즘 풀이

---
+++ Baekjoon/24479.py
@@ -0,0 +1,61 @@
+import sys
+from collections import deque
+
+N, M, R = map(int, sys.stdin.readline().split())
+adj = [[] for _ in range(N+1)]
+for _ in range(M):
+    src, dst = map(int, sys.stdin.readline().split())
+    adj[src].append(dst)
+    adj[dst].append(src)
+for x in range(1, N+1):
+    adj[x] = sorted(adj[x], reverse = True)
+
+visited = [0] * (N+1)
+nodes_cnt = [0] * (N+1)
+order = 1  # 방문순서 체크 변수
+stack = deque()
+stack.append(R)
+
+
+while stack:
+    cur_node = stack.pop()
+    visited[cur_node] = 1
+    if nodes_cnt[cur_node] == 0:
+        nodes_cnt[cur_node] = order
+        order += 1
+
+    for next_node in adj[cur_node]:
+        if visited[next_node] == 0:
+            stack.append(next_node)
+
+print(*nodes_cnt[1:])
+
+# import sys
+# sys.setrecursionlimit(50000)
+
+# N, M, R = map(int, sys.stdin.readline().split())
+# adj = [[] for _ in range(N+1)]
+# for _ in range(M):
+#     src, dst = map(int, sys.stdin.readline().split())
+#     adj[src].append(dst)
+#     adj[dst].append(src)
+# for x in range(1, N):
+#     adj[x] = sorted(adj[x])
+
+# # print(adj)
+# visited = [0] * (N+1)
+# order = 1  # 방문순서 체크 변수
+
+# def dfs(graph, start):
+#     global visited, order
+#     visited[start] = order
+#     order += 1
+#     # print(f'visit {start}!')
+
+#     for i in graph[start]:
+#         # print(f'start:{start}')
+#         if visited[i] == 0:
+#             dfs(graph, i)
+
+# dfs(adj, 1)
+# print(*visited[1:])
